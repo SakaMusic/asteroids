04ecc696b1dbbddaaa2aa363d0d4501199abc061
SakaMusic <wshbenson@gmail.com>
2026-02-19 11:48:34 -0700

    Created game loop and screen

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    screen.display.flip()
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000
+        screen.fill("black")
+        pygame.display.flip()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f'Screen height: {SCREEN_HEIGHT}')
